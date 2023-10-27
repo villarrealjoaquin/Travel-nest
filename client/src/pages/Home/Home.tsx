f2f8d9dae2dbe28b7abcf7bdbdfb38fa48919541
Santiago Muscolo <smuscolo2003@gmail.com>
2023-10-26 21:29:37 -0300

    why choose us section added, necesitamos hacer una imagen svg para el fondo

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,6 +5,7 @@ import apartment3 from '/apartment3.webp';
 import location from '/svgs/location.svg';
 import { ApartmentSearch } from "."
 import SliderHome from './components/Slider/Slider';
+import { WhyChooseUs } from './components/WhyChooseUs/WhyChooseUs';
 
 function Home() {
 
@@ -43,29 +44,33 @@ function Home() {
   const currentApartment = apartments[currentApartmentIndex];
 
   return (
-    <main>
+    <main >
       <ApartmentSearch />
-        <div className='flex flex-col justify-center'>
-          <div className="w-full h-[700px] bg-black opacity-40 z-10"></div>
-          {apartments.map((apartment, index) => (
-            <img
-              key={apartment.id}
-              src={apartment.image}
-              alt=''
-              className={`absolute top-[175px] left-0 w-full h-[700px] transition-opacity duration-500 ${index === currentApartmentIndex ? 'opacity-100' : 'opacity-0'
-                }`}
-              style={{ zIndex: index === currentApartmentIndex ? 1 : 0 }}
-            />
-          ))}
-          <div className='flex flex-col absolute top-[50%] left-[5%] z-20'>
-            <span className='text-white text-[4rem] max-w-4xl mb-5 font-bold'>tu hogar en cualquier lugar, descubre, conecta, vive.</span>
-            <div className='flex items-center'>
-              <img src={location} alt="" className='text-black' width={40} />
-              <p className='text-white text-[1.4rem]  pl-2'>{currentApartment.info} <span className='font-bold'>{currentApartment.highlighted}</span></p>
-            </div>
+      <div className='flex flex-col justify-center'>
+        <div className="w-full h-[700px] bg-black opacity-40 z-10"></div>
+        {apartments.map((apartment, index) => (
+          <img
+            key={apartment.id}
+            src={apartment.image}
+            alt=''
+            className={`absolute top-[175px] left-0 w-full h-[700px] transition-opacity duration-500 ${index === currentApartmentIndex ? 'opacity-100' : 'opacity-0'
+              }`}
+            style={{ zIndex: index === currentApartmentIndex ? 1 : 0 }}
+          />
+        ))}
+        <div className='flex flex-col absolute top-[50%] left-[5%] z-20'>
+          <span className='text-white text-[4rem] max-w-4xl mb-5 font-bold'>tu hogar en cualquier lugar, descubre, conecta, vive.</span>
+          <div className='flex items-center'>
+            <img src={location} alt="" className='text-black' width={40} />
+            <p className='text-white text-[1.4rem]  pl-2'>{currentApartment.info} <span className='font-bold'>{currentApartment.highlighted}</span></p>
           </div>
         </div>
-        <SliderHome title='travel everywhere' containerClassNames="z-10 bg-white" />
+      </div>
+      <SliderHome title='travel everywhere' containerClassNames="z-10 bg-white" />
+      <section className='overflow-hidden'>
+        <WhyChooseUs />
+      </section>
+
     </main>
   );
 }
